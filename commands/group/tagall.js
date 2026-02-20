267f163f5e550fcf5fd7f3129c5131a5de45b3dc
jerem146 <vasquezever146@gmail.com>
2026-02-20 11:08:39 -0500

    Refactor group tag message format

--- commands/group/tagall.js
+++ commands/group/tagall.js
@@ -6,12 +6,25 @@ export default {
     const groupInfo = await client.groupMetadata(m.chat)
     const participants = groupInfo.participants
     const pesan = args.join(' ')
-    let teks = `﹒⌗﹒🌱 .ৎ˚₊‧  ${pesan || 'Revivan 🪴'}\n\n𐚁 ֹ ִ \`GROUP TAG\` ! ୧ ֹ ִ🍃\n\n🍄 \`Miembros :\` ${participants.length}\n🌿 \`Solicitado por :\` @${m.sender.split('@')[0]}\n\n` +
-      `╭┄ ꒰ \`Lista de usuarios:ׄ\` ꒱ ┄\n`
+
+    let teks = `╭━━〔 📣 𝗖𝗢𝗡𝗩𝗢𝗖𝗔𝗧𝗢𝗥𝗜𝗔 〕━━⬣\n`
+    teks += `┃ ✦ Mensaje : ${pesan || 'Todos presentes ⚔️'}\n`
+    teks += `┃ ✦ Miembros : ${participants.length}\n`
+    teks += `┃ ✦ Invoca : @${m.sender.split('@')[0]}\n`
+    teks += `╰━━━━━━━━━━━━━━━━⬣\n\n`
+
+    teks += `╭━━〔 👥 𝗟𝗜𝗦𝗧𝗔 𝗗𝗘 𝗠𝗜𝗘𝗠𝗕𝗥𝗢𝗦 〕━━⬣\n`
     for (const mem of participants) {
-      teks += `┊ꕥ @${mem.id.split('@')[0]}\n`
+      teks += `┃ ➤ @${mem.id.split('@')[0]}\n`
     }
-    teks += `╰⸼ ┄ ┄ ꒰ \`${version}\` ꒱ ┄ ┄⸼`
-    return client.reply(m.chat, teks, m, { mentions: [m.sender, ...participants.map(p => p.id)] })
+    teks += `╰━━━━━━━━━━━━━━━━⬣\n`
+    teks += `▸ ${version}`
+
+    return client.reply(
+      m.chat,
+      teks,
+      m,
+      { mentions: [m.sender, ...participants.map(p => p.id)] }
+    )
   }
 }
